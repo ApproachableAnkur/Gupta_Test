0f40ac7d165f44b2a19ac2ac7eee5a37b355859c
ankur gupta <ankur.p.gupta1989@gmail.com>
2020-12-01 07:30:44 -0800

    Version 3

--- AnkitaTest.java
+++ AnkitaTest.java
@@ -2,7 +2,7 @@ class AnkitaTest
 {
 public static void main (String args[])
 {
-int a=80,b=80,c;
+int a=80,b=90,c;
 c=a+b;
 System.out.println ("The first number is :"+a);
 System.out.println ("The second number is :"+b);
